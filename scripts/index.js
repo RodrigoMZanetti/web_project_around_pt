3b51d2cb1b634fa4100b624501c71874bad728fe
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2026-01-05 11:42:55 +0100

    feat: modified script

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,62 @@ const initialCards = [
 initialCards.forEach(function (item) {
   console.log(item.name);
 });
+
+const modalGeral = document.querySelector("#edit-popup");
+
+const editButton = document.querySelector(".profile__edit-button");
+editButton.addEventListener("click", function () {
+  handleOpenEditModal(modalGeral);
+});
+
+function handleOpenEditModal(modalGeral) {
+  fillProfileForm(modalGeral);
+  openModal(modalGeral);
+}
+
+function fillProfileForm(modalGeral) {
+  const nameElement = document.querySelector(".profile__title");
+  const nameElementInput = modalGeral.querySelector(".popup__input_type_name");
+  const nameElementText = nameElement.textContent;
+  nameElementInput.value = nameElementText;
+
+  const experienceElement = document.querySelector(".profile__description");
+  const experienceText = experienceElement.textContent;
+  const experienceInput = modalGeral.querySelector(
+    ".popup__input_type_description"
+  );
+  experienceInput.value = experienceText;
+}
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+let formElement = document.querySelector("#edit-profile-form");
+formElement.addEventListener("submit", function (evt) {
+  handleProfileFormSubmit(evt);
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = modalGeral.querySelector(".popup__input_type_name");
+  const nameInputValue = nameInput.value;
+  const nameElement = document.querySelector(".profile__title");
+  nameElement.textContent = nameInputValue;
+
+  let jobInput = modalGeral.querySelector(".popup__input_type_description");
+  const jobInputValue = jobInput.value;
+  const experienceElement = document.querySelector(".profile__description");
+  experienceElement.textContent = jobInputValue;
+
+  closeModal(modalGeral);
+}
+
+const closeButton = modalGeral.querySelector(".popup__close");
+closeButton.addEventListener("click", function () {
+  closeModal(modalGeral);
+});
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
