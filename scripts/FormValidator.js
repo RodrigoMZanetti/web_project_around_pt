7eb9eac005ddab90569a2bb55f557a6906cf745a
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2026-02-22 10:23:20 -0300

    feat: create section class

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -53,4 +53,25 @@ class FormValidator {
   }
 }
 
+/////SPRINT 11 - CLASS SECTION CREATION
+
+class Section {
+  constructor({ items, renderer }, selector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._selector = selector;
+  }
+
+  render() {
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    const container = document.querySelector(this._selector);
+    container.append(element);
+  }
+}
+
 export default FormValidator;
