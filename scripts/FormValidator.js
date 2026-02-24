969702093ecfe0286970c18e136287637c353768
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2026-02-24 17:36:09 -0300

    feat: create class UserInfo

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -164,5 +164,25 @@ const popImageClass = new PopupWithForm(
   handleCardFormSubmit,
 );
 
+//Criação da classe UserInfo
+class UserInfo {
+  constructor({ inputNameSelector, inputJobSelector }) {
+    this._inputName = document.querySelector(inputNameSelector);
+    this._inputJob = document.querySelector(inputJobSelector);
+  }
+
+  getUserInfo() {
+    const userInfo = {
+      name: this._inputName.textContent,
+      job: this._inputJob.textContent,
+    };
+    return userInfo;
+  }
+
+  setUserInfo(userInfo) {
+    this._inputName.textContent = userInfo.name;
+    this._inputJob.textContent = userInfo.job;
+  }
+}
 /////////////
 export default FormValidator;
